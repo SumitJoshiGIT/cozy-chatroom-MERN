58e39ad7725cacc75009f6a3cd48ba548f381923
Sumit Joshi <git.sumitjoshi@gmail.com>
2024-08-24 01:06:19 +0530

    upgrading the ui styling

--- server/client/src/components/AuthScreen/SignIn/SignIn.jsx
+++ server/client/src/components/AuthScreen/SignIn/SignIn.jsx
@@ -13,20 +13,20 @@ export default function Signup(){
       }
    return (
     
-    <div className="flex justify-center items-center flex-col">
+    <div className="flex bg-white p-10 rounded-lg  shadow-lg justify-center items-center flex-col">
      <div className="text-3xl mb-2">
-        <h2>Welcome Back</h2>
+        <h2 className="mb-2 font-bold">Welcome Back</h2>
      </div>
-     <button className='bg-blue-400 shadow shadow-2 p-1 rounded-md '>
+     <button className='bg-blue-400 shadow-md font-bold p-1 pl-3 pr-3 rounded-full  '>
          Signin with Google
      </button>
-     <div className="m-1 text-sm">Or</div>
-     <input value={value} onChange={(event)=>setValue(event.target.value)} placeholder={"Enter your email address"} className="rounded-md  mt-1 pl-2 pr-4 text-ellipses p-1 border-2" type="text"></input>
-     <input value={pass} onChange={(event)=>setPass(event.target.value)} placeholder={"Enter your password"} className=" rounded-md  mt-1 pl-2 pr-4 text-ellipses p-1 border-2" type="text"></input>
+     <div className="m-1 text-sm font-semibold">Or</div>
+     <input value={value} onChange={(event)=>setValue(event.target.value)} placeholder={"Enter your email address"} className="rounded-full border font-roboto   mt-1 pl-4 pr-4 text-ellipses p-1 text-gray-600 bg-gray-100 " type="text"></input>
+     <input value={pass} onChange={(event)=>setPass(event.target.value)} placeholder={"Enter your password"} className=" rounded-full font-roboto   mt-2 mb-2 pl-4 pr-4 text-ellipses p-1 border text-gray-600 bg-gray-100" type="text "></input>
     
      <iframe src="/oauth/callback" class="google-auth hidden" style={{position:'absolute',width:'100vw',height:'100vh'}}>
      </iframe>
-     <button className="mt-2 bg-black text-white p-1 pl-2 pr-2 rounded-md mb-2" onClick={onClick}>Continue </button>
+     <button className="mt-2 bg-black text-white p-1 pl-3 pr-3 rounded-full mb-2" onClick={onClick}>Continue </button>
      <div className="text-base">Don't have an account? <Link to="/auth/signup">SignUp</Link></div>
     </div>
  )
--- server/client/src/components/AuthScreen/SignUp/SignUp.jsx
+++ server/client/src/components/AuthScreen/SignUp/SignUp.jsx
@@ -45,19 +45,19 @@ export default function Signup(){
      ,[])
 
  return (
-    <div className="flex justify-center items-center flex-col">
+    <div className="flex justify-center items-center flex-col bg-white p-10 rounded-lg shadow-lg">
      <div className="text-3xl mb-2">
-        <h2>Thank you </h2>
+        <h2 className="font-bold mb-2">Welcome Aboard </h2>
      </div>
      <div className="flex flex-col mb-2">
-     <input placeholder="Enter your email address" className=" rounded-md  mt-2 pl-2 pr-4 text-ellipses p-1 border-2"  onChange={(e)=>setEmail(e.target.value)} value={email}></input>
-     <input placeholder="Enter your password" className=" pl-2 pr-4 text-ellipses rounded-md p-1 mt-2 border-2"  onChange={(e)=>setPassword(e.target.value)} value={password} />
-     <input placeholder="Confirm your password" className=" rounded-md  pl-2 pr-4 text-ellipses p-1 mt-2 border-2"  onChange={(e)=>setConfirmPassword(e.target.value)} value={confirmPassword} />
+     <input placeholder="Enter your email address" className=" shadow rounded-full font-roboto   mt-1 mb-2 pl-4 pr-4 text-ellipses p-1 border text-gray-600 bg-gray-100"  onChange={(e)=>setEmail(e.target.value)} value={email}></input>
+     <input placeholder="Enter your password" className=" shadow rounded-full font-roboto   mt-1 mb-2 pl-4 pr-4 text-ellipses p-1 border text-gray-600 bg-gray-100"  onChange={(e)=>setPassword(e.target.value)} value={password} />
+     <input placeholder="Confirm your password" className=" shadow rounded-full font-roboto   mt-1 mb-2 pl-4 pr-4 text-ellipses p-1 border text-gray-600 bg-gray-100"  onChange={(e)=>setConfirmPassword(e.target.value)} value={confirmPassword} />
      </div>
-     <button className='mt-2 p-1 bg-black text-white w-fit rounded-md shadow text-sm' onClick={SignUp}>Continue</button>
-     <div className="text-sm m-1 w-full items-center flex justify-center">Or</div>
+     <button className='mt-2 p-1  bg-black text-white shadow w-fit rounded-full pl-3 pr-3 shadow text-sm' onClick={SignUp}>Continue</button>
+     <div className="text-sm m-1 w-full items-center font-semibold flex justify-center">Or</div>
  
-     <Link className=" bg-blue-400 p-1 rounded-md "  to="/auth/signin">
+     <Link className=" bg-blue-400 pl-3 pr-3 mb-3 shadow rounded-full p-1 "  to="/auth/signin">
            SignIn with Google
      </Link>
      <div className="text-base">Already have an account? <Link to="/auth/signIn">SignIn</Link></div>
